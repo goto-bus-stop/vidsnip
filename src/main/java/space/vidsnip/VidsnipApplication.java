4f55b50571181bfe019f46f88264ec97afef6f06
René Kooi <renee@kooi.me>
2016-06-14 13:12:01 +0200

    add checkstyle

--- src/main/java/space/vidsnip/VidsnipApplication.java
+++ src/main/java/space/vidsnip/VidsnipApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VidsnipApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(VidsnipApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VidsnipApplication.class, args);
+    }
 }
