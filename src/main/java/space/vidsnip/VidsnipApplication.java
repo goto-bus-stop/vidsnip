aac6d7bf24b8c99bf05c27bc701316315ab35c44
René Kooi <renee@kooi.me>
2016-06-14 12:46:40 +0200

    init, closes #1

---
+++ src/main/java/space/vidsnip/VidsnipApplication.java
@@ -0,0 +1,11 @@
+package space.vidsnip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidsnipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VidsnipApplication.class, args);
+	}
+}
