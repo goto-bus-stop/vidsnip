0e2fb361739d59b4b2a8c233380cc79738cc9f74
Marijn Pool <info@marijnpool.nl>
2016-06-23 01:04:10 +0200

    Personalized timeline works

--- src/main/java/space/vidsnip/model/snip/SnipRepository.java
+++ src/main/java/space/vidsnip/model/snip/SnipRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import org.springframework.security.access.method.P;
 import space.vidsnip.model.user.User;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "snips", path = "snips")
 public interface SnipRepository extends PagingAndSortingRepository<Snip, Long> {
     Page<Snip> findByAuthorUsername(@Param("user") String username, Pageable pageable);
     Page<Snip> findByAuthor(User user, Pageable pageable);
+    Page<Snip> findByAuthorInOrderByCreatedAtDesc(List<User> users, Pageable pageable);
 }
