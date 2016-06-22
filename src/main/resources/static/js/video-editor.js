1124b9d788f6debf437f589466c56b905345b46f
René Kooi <renee@kooi.me>
2016-06-22 15:18:32 +0200

    implement selecting a video from search results for a new Snip

--- src/main/resources/static/js/video-editor.js
+++ src/main/resources/static/js/video-editor.js
@@ -1,4 +1,5 @@
 var $ = require('jquery')
+var youtubePlayer = require('youtube-player')
 
 function resultTemplate (video) {
     var thumbnail = $('<img />')
@@ -12,33 +13,71 @@ function resultTemplate (video) {
         $('<div />').css({ 'max-height': '100px', overflow: 'hidden' })
             .append(title, description)
     )
-    return $('<li />').addClass('media').append(left, body)
+    return $('<li />')
+        .addClass('media video-search-result')
+        .data('video', video)
+        .append(left, body)
 }
 
 module.exports = function (editor) {
     editor = $(editor)
-    var query = editor.find('.video-search-query input')
-    var results = editor.find('.video-search-results')
-    var loader = editor.find('.video-search-loading')
+
+    var $query = editor.find('.video-search-query input')
+    var $results = editor.find('.video-search-results')
+    var $loader = editor.find('.video-search-loading')
+    var $video = editor.find('[data-hook="video-editor-video"]')
+
+    var player = youtubePlayer($video.children()[0], {
+        playerVars: {
+            controls: 0,       // no controls
+            rel: 0,            // do not show related videos after the video finishes
+            showinfo: 0,       // do not show video title etc in the frame
+            iv_load_policy: 3, // disable annotations
+            modestbranding: 1  // hide youtube logo
+        }
+    })
 
     var request
     var requestTimer
-    query.on('input', function () {
-        loader.addClass('is-visible')
+    $query.on('input', function () {
+        $loader.addClass('is-visible')
         clearTimeout(requestTimer)
         if (request) {
             request.abort()
             request = null
         }
         requestTimer = setTimeout(function () {
-            request = $.getJSON('/new/search', { q: query.val() })
+            request = $.getJSON('/new/search', { q: $query.val() })
             request.done(function (result) {
-                request = null
-                loader.removeClass('is-visible')
-                results.empty().append(
+                $request = null
+                $loader.removeClass('is-visible')
+                $results.empty().append(
                     result.map(resultTemplate)
                 )
             })
         }, 100)
     })
+
+    $results.on('click', '.media', function (e) {
+        var item = $(e.currentTarget)
+        var video = item.data('video')
+        selectVideo(video)
+    })
+
+    function selectVideo (video) {
+        $results.children().each(function () {
+            var v = $(this).data('video')
+            if (v === video) {
+                $(this).addClass('is-selected')
+            } else {
+                $(this).removeClass('is-selected')
+            }
+        })
+
+        if (video) {
+            player.loadVideoById(video.id)
+        } else {
+            player.stopVideo();
+        }
+    }
 }
